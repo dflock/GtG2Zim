48ebc13469cd73c5c867a247d274bf5a430c9ad2
Duncan Lock <duncan.lock@gmail.com>
2013-07-28 01:40:20 -0700

    Added basic Zim Notebook creation; comments skeleton for rest

--- gtg2zim.py
+++ gtg2zim.py
@@ -3,7 +3,7 @@
 """GtG2Zim - Exports GTG tasks to Zim Desktop Wiki pages.
 
 Usage:
-    gtg2zim.py TAG [-o PATH]
+    gtg2zim.py TAG [-i PATH] [-o PATH]
     gtg2zim.py (-h | --help)
     gtg2zim.py --version
 
@@ -12,24 +12,60 @@ Arguments:
 
 Options:
     -o PATH, --output=PATH    The output folder to save the Zim pages to. [default: ./]
+    -i PATH, --input=PATH     The input folder where the gtg files are. [default: ~/.local/share/gtg]
     -h --help                 Show this screen.
     --version                 Show version.
 
 """
 from docopt import docopt
 import os
+from datetime import datetime
 
 
-def gtg2zim(tag, output_path):
-    pass
+def create_zim_notebook(name, output_path):
+    """Create a ZIM Notebook in the output folder, with the given name."""
+
+    # Make a notebook folder inside output_path
+    notebook_folder = os.path.join(output_path, name.replace(' ', '_'))
+    if not os.path.exists(notebook_folder):
+        os.makedirs(notebook_folder)
+
+    # Create the notebook.zim index file
+    nb = '[Notebook]\nname={0}\nversion=0.4\nendofline=unix\nprofile=None\n'.format(name)
+    with open(os.path.join(notebook_folder, 'notebook.zim'), 'w+') as f:
+        f.write(nb)
+
+    # Create notebook file
+    d = datetime.now()
+    nb = 'Content-Type: text/x-zim-wiki\nWiki-Format: zim 0.4\nCreation-Date: {:%Y-%m-%dT%H:%M:%S%z}\n\n====== {} ======\nCreated {:%A %d %B %Y}\n'.format(d, name, d)
+    with open(os.path.join(notebook_folder, name.replace(' ', '_') + '.txt'), 'w+') as f:
+        f.write(nb)
+
+
+def gtg2zim(tag, input_path, output_path):
+    print tag, output_path, input_path
+    create_zim_notebook('Test Notebook', output_path)
+
+    # Check for existence of gtg files in input_path:
+    #   tags.xml
+    #   projects.xml
+    #   gtg_tasks-*.xml
+    #
+    # Read in tags.xml
+    # For each tag, create a ZIM Notebook in the output folder.
+    #
+    # Read in gtg projects.xml file
+    #
+    # For each <backend..> where module="backend_localfile", read in the file pointed to by the path attribute
 
 if __name__ == '__main__':
     args = docopt(__doc__, version='GtG2Zim 0.1')
 
-    if args['--output']:
-        try:
-            output_path = os.path.isdir(args['--output'])
-            gtg2zim(args['TAG'], output_path)
+    # print args
 
-        except IOError, e:
-            print e
+    if args['--output'] and args['--input']:
+        if os.path.isdir(os.path.expanduser(args['--output'])) and os.path.isdir(os.path.expanduser(args['--input'])):
+            # TODO: Check that we can write to output folder
+            output_path = os.path.expanduser(args['--output'])
+            input_path = os.path.expanduser(args['--input'])
+            gtg2zim(args['TAG'], input_path, output_path)
